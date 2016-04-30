1fa2ddc5359ab85b4322ce71e54c775e2897f840
distres117 <omcrobbie@gmail.com>
2016-04-30 09:41:21 -0400

    closes #73

--- browser/js/common/directives/product-pane/productPane.js
+++ browser/js/common/directives/product-pane/productPane.js
@@ -10,6 +10,16 @@ app.directive('productPane', function(){
       $scope.addToCart = function(product){
         return CartFactory.addToCart(product, 1);
       };
+
+      $scope.nInCart = function(product){
+        if (CartFactory.isCart()){
+          var match = CartFactory.fetchCart().items.filter(function(it){
+            return it.product._id === product._id;
+          })[0];
+          return  match ? match.quantity : 0;
+        }
+        return;
+      };
     },
     templateUrl: 'js/common/directives/product-pane/product-pane.html'
   };
