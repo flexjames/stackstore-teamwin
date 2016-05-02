79a9e83da5bbebeade01bd473306c3d6bd38cf4a
Jonah Wolf <wolfjonah@gmail.com>
2016-05-01 22:23:05 -0400

    Working e-mail notifications

--- server/app/routes/orders/index.js
+++ server/app/routes/orders/index.js
@@ -5,55 +5,66 @@ var _ = require('lodash');
 var mongoose = require('mongoose');
 var Order = mongoose.model('Order');
 
-router.get('/', function(req, res,next){
+router.get('/', function(req, res, next) {
   //returns a new order
   var order = new Order();
   res.json(order);
 });
 
-router.get('/:userId', function(req,res,next){
-  Order.find({user: req.params.userId, status: 'created' })
-  .then(function(orders){
-    res.json(orders);
-  }, next);
+router.get('/:userId', function(req, res, next) {
+  Order.find({ user: req.params.userId, status: 'created' })
+    .then(function(orders) {
+      res.json(orders);
+    }, next);
 });
 
-router.put('/:orderId', function(req,res, next){
-  Order.findById(req.params.orderId).then(function(order){
-    if (order){
+router.put('/:orderId', function(req, res, next) {
+  Order.findById(req.params.orderId).then(function(order) {
+    if (order) {
       order.items = req.body.items;
-      order.save().then(function(){
+      order.save().then(function() {
         res.json(order);
       }, next);
     } else {
       res.json(order);
-  }
+    }
   });
 });
 
-router.post('/', function(req,res,next){
+router.post('/', function(req, res, next) {
   //Creates a new order from the recieved data
-  Order.create(req.body).then(function(order){
+  Order.create(req.body).then(function(order) {
     res.json(order);
   }, next);
 });
 
-router.put('/checkout/:orderId', function(req,res,next){
-  Order.findById(req.params.orderId).then(function(order){
-    order.status = req.body.status;
-    order.email = req.body.email;
-    order.address = req.body.address;
-    order.save().then(function(){
-      res.sendStatus(200);
-    });
+router.put('/checkout/:orderId', function(req, res, next) {
+Order.findById(req.params.orderId).then(function(order) {
+    if (order) {
+      order.status = req.body.status;
+      order.email = req.body.email;
+      order.address = req.body.address;
+      return order;
+    } else {
+      return Order.create(req.body);
+    }
+  })
+  .then(function(order) {
+    return order.save();
+  })
+  .then(function(order) {
+    return order.confirm();
+  })
+  .then(function() {
+    res.sendStatus(200);
   });
 });
 
-router.delete('/:orderId', function(req,res,next){
-  Order.remove({_id: req.params.orderId})
-  .then(function(){
-    res.sendStatus(203);
-  },next);
+router.delete('/:orderId', function(req, res, next) {
+  Order.remove({ _id: req.params.orderId })
+    .then(function() {
+      res.sendStatus(203);
+    }, next);
 });
 
 router.post('/addItem', function(req, res, next) {
@@ -89,7 +100,7 @@ router.post('/removeItem', function(req, res) {
 
 router.post('/commit', function(req, res, next) {
   Order.findById(req.session.order)
-    .then(function(order){
+    .then(function(order) {
       return order.commit();
     })
     .then(function(order) {
