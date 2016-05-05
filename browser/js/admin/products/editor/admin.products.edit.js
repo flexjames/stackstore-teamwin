a67c6b1ad757e77ef1383fdce85290b46f18b17f
distres117 <omcrobbie@gmail.com>
2016-05-05 14:32:39 -0400

    category editing

--- browser/js/admin/products/editor/admin.products.edit.js
+++ browser/js/admin/products/editor/admin.products.edit.js
@@ -3,12 +3,26 @@ app.config(function($stateProvider){
     .state('admin.productsEdit',{
       url: '/products/:productId',
       templateUrl: '/js/admin/products/editor/admin-product-edit.html',
-      controller: function(product, $scope){
+      controller: function(product, categories, $scope){
         $scope.product = product;
+        $scope.categories = categories;
+        $scope.checkCategory = function(category){
+          return $scope.product.category.indexOf(category._id) > -1;
+        };
+        $scope.onChange = function(category){
+          var idx = $scope.product.category.indexOf(category._id);
+          if (idx === -1)
+            $scope.product.category.push(category._id);
+          else
+            $scope.product.category.splice(idx, 1);
+        };
       },
       resolve: {
         product:function(ProductsFactory, $stateParams){
           return ProductsFactory.fetchById($stateParams.productId);
+        },
+        categories: function(CategoryFactory){
+          return CategoryFactory.fetchAll();
         }
       }
 
